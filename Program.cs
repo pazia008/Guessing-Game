c3f5693abb73fb5f20d3251f0f10bb3951b1facc
Pazia Ramirez <pazia008@gmail.com>
2021-03-02 13:20:06 -0600

    added what guess you are on

--- Program.cs
+++ Program.cs
@@ -11,21 +11,25 @@ namespace GuessingGame
 
 
             int SecretNumber = 42;
-            int CurrentGuesses = 0;
+            int CurrentGuesses = 1;
 
             void Attempt()
             {
-                while (CurrentGuesses < 4)
+                while (CurrentGuesses < 5)
                 {
                     string Guess = Console.ReadLine();
                     int GuessNumbers = Int32.Parse(Guess);
+
                     if (GuessNumbers == SecretNumber)
                     {
                         Console.WriteLine("PROUD OF YOU!!");
+                        Console.WriteLine($"you are on guess {CurrentGuesses}");
+                        break;
                     }
                     else
                     {
                         Console.WriteLine("YOU have FAILED!");
+                        Console.WriteLine($"you are on guess {CurrentGuesses}");
                         CurrentGuesses++;
 
                     };
