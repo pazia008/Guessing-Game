b0ffefd705db2a1d52fdc4f0a88a43d4342bbe8c
Pazia Ramirez <pazia008@gmail.com>
2021-03-02 10:22:28 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Can you guess the seCreT number?");
+            Console.WriteLine("--------------------------------");
+
+            string Guess = Console.ReadLine();
+            int GuessNumbers = Int32.Parse(Guess);
+            int SecretNumber = 69;
+
+            void Attempt()
+            {
+                while (GuessNumbers != SecretNumber)
+                {
+                    if (GuessNumbers == SecretNumber)
+                    {
+                        Console.WriteLine("PROUD OF YOU");
+                    }
+                    else
+                    {
+                        Console.WriteLine("YOU have FAILED!");
+                    };
+                }
+
+            }
+            Attempt();
+        }
+    }
+}
