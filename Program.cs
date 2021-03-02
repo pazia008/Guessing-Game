5266688aeb3413f0dc8ae13453e3b0eda659a5a0
Pazia Ramirez <pazia008@gmail.com>
2021-03-02 15:00:22 -0600

    done with phase 7

--- Program.cs
+++ Program.cs
@@ -9,14 +9,17 @@ namespace GuessingGame
             Console.WriteLine("Can you guess the seCreT number?");
             Console.WriteLine("--------------------------------");
 
-
             var random = new Random();
             int SecretNumber = random.Next(1, 100);
             int CurrentGuesses = 1;
 
-            void Attempt()
+            string Level = "Choose a levle: EASY, MEDIUM or HARD (E/M/H)";
+            Console.Write($"{Level}");
+            string LevelAnswer = Console.ReadLine().ToLower();
+
+            if (LevelAnswer == "e")
             {
-                while (CurrentGuesses < 5)
+                while (CurrentGuesses < 9)
                 {
                     string Guess = Console.ReadLine();
                     int GuessNumbers = Int32.Parse(Guess);
@@ -25,14 +28,15 @@ namespace GuessingGame
                     {
                         Console.WriteLine("PROUD OF YOU!!");
                         Console.WriteLine($"you are on guess {CurrentGuesses}");
-                        Console.WriteLine($"({CurrentGuesses}/4)");
+                        Console.WriteLine($"({CurrentGuesses}/8)");
                         break;
+
                     }
                     else if (GuessNumbers > SecretNumber)
                     {
                         Console.WriteLine("YOU have FAILED!");
                         Console.WriteLine($"you are on guess {CurrentGuesses}");
-                        Console.WriteLine($"({CurrentGuesses}/4)");
+                        Console.WriteLine($"({CurrentGuesses}/8)");
                         Console.WriteLine("....Your Guess Was Too High");
                         CurrentGuesses++;
 
@@ -41,17 +45,84 @@ namespace GuessingGame
                     {
                         Console.WriteLine("YOU have FAILED!");
                         Console.WriteLine($"you are on guess {CurrentGuesses}");
-                        Console.WriteLine($"({CurrentGuesses}/4)");
+                        Console.WriteLine($"({CurrentGuesses}/8)");
                         Console.WriteLine("....Your Guess Was Too Low");
                         CurrentGuesses++;
 
                     };
 
                 }
-
-
             }
-            Attempt();
+            else if (LevelAnswer == "m")
+            {
+                while (CurrentGuesses < 7)
+                {
+                    string Guess = Console.ReadLine();
+                    int GuessNumbers = Int32.Parse(Guess);
+
+                    if (GuessNumbers == SecretNumber)
+                    {
+                        Console.WriteLine("PROUD OF YOU!!");
+                        Console.WriteLine($"you are on guess {CurrentGuesses}");
+                        Console.WriteLine($"({CurrentGuesses}/6)");
+                        break;
+
+                    }
+                    else if (GuessNumbers > SecretNumber)
+                    {
+                        Console.WriteLine("YOU have FAILED!");
+                        Console.WriteLine($"you are on guess {CurrentGuesses}");
+                        Console.WriteLine($"({CurrentGuesses}/6)");
+                        Console.WriteLine("....Your Guess Was Too High");
+                        CurrentGuesses++;
+
+                    }
+                    else if (GuessNumbers < SecretNumber)
+                    {
+                        Console.WriteLine("YOU have FAILED!");
+                        Console.WriteLine($"you are on guess {CurrentGuesses}");
+                        Console.WriteLine($"({CurrentGuesses}/6)");
+                        Console.WriteLine("....Your Guess Was Too Low");
+                        CurrentGuesses++;
+
+                    };
+                }
+            }
+            else if (LevelAnswer == "h")
+            {
+                while (CurrentGuesses < 4)
+                {
+                    string Guess = Console.ReadLine();
+                    int GuessNumbers = Int32.Parse(Guess);
+
+                    if (GuessNumbers == SecretNumber)
+                    {
+                        Console.WriteLine("PROUD OF YOU!!");
+                        Console.WriteLine($"you are on guess {CurrentGuesses}");
+                        Console.WriteLine($"({CurrentGuesses}/3)");
+                        break;
+
+                    }
+                    else if (GuessNumbers > SecretNumber)
+                    {
+                        Console.WriteLine("YOU have FAILED!");
+                        Console.WriteLine($"you are on guess {CurrentGuesses}");
+                        Console.WriteLine($"({CurrentGuesses}/3)");
+                        Console.WriteLine("....Your Guess Was Too High");
+                        CurrentGuesses++;
+
+                    }
+                    else if (GuessNumbers < SecretNumber)
+                    {
+                        Console.WriteLine("YOU have FAILED!");
+                        Console.WriteLine($"you are on guess {CurrentGuesses}");
+                        Console.WriteLine($"({CurrentGuesses}/3)");
+                        Console.WriteLine("....Your Guess Was Too Low");
+                        CurrentGuesses++;
+
+                    };
+                };
+            }
         }
     }
 }
